7af7613a29cb521e0942967b4031ba45be819775
Jirayu-CT <125994181+Jirayu-CT@users.noreply.github.com>
2023-02-22 22:56:49 +0700

    Update Exercise4_Jirayu_C.py

--- Exercise4_Jirayu_C.py
+++ Exercise4_Jirayu_C.py
@@ -4,7 +4,7 @@ general_Business = 87.53
 introduction_To_Computer_Systems = 90
 computer_Programming = 100
 
-print("---Your Score")
+print("---Your Score---")
 print("Foundation English :", foundation_English)
 print("General Business :", general_Business)
 print("Introduction to Computer Systems :", introduction_To_Computer_Systems)
