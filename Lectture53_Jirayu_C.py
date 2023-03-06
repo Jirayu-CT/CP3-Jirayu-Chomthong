7f285b572637db579bd43c0f9bc9163454356cde
Jirayu-CT <125994181+Jirayu-CT@users.noreply.github.com>
2023-03-06 17:59:24 +0700

    Update Lectture53_Jirayu_C.py

--- Lectture53_Jirayu_C.py
+++ Lectture53_Jirayu_C.py
@@ -2,8 +2,7 @@ price = int(input("Enter Price :"))
 
 
 def VatCalculate(price):
-    result = price + (price*7/100)
-    return result
+    return price + (price*7/100)
 
 
 print(VatCalculate(price))
