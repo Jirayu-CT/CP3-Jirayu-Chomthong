6ba9f4d99c8a82d5ba65e5d945b7218441a473d3
Jirayu-CT <125994181+Jirayu-CT@users.noreply.github.com>
2023-03-06 18:03:23 +0700

    Update Lectture53_Jirayu_C.py

--- Lectture53_Jirayu_C.py
+++ Lectture53_Jirayu_C.py
@@ -1,8 +1,13 @@
-price = int(input("Enter Price :"))
+print("---------------------")
+print("---------------------")
+price = int(input("Price :"))
+print("---------------------")
 
 
 def VatCalculate(price):
     return price + (price*7/100)
 
 
-print(VatCalculate(price))
+print("Total Price = %s Baht" % VatCalculate(price))
+print("---------------------")
+print("---------------------")
