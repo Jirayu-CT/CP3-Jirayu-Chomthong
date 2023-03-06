28c2f5f7ce1ac2930e2b015f4ecf968c44d3f1a0
Jirayu-CT <125994181+Jirayu-CT@users.noreply.github.com>
2023-03-06 17:37:47 +0700

    Update Lectture50_Jirayu_C.py

--- Lectture50_Jirayu_C.py
+++ Lectture50_Jirayu_C.py
@@ -18,13 +18,13 @@ def MultiplyNumber(number1, number2):
     print("%s x %s = %s" % (number1, number2, (number1*number2)))
 
 
-def DibideNumber(number1, number2):
+def DivideNumber(number1, number2):
     print("%s / %s = %s" % (number1, number2, (number1/number2)))
 
 
 AddNumber(number1, number2)
 SubtractNumber(number1, number2)
 MultiplyNumber(number1, number2)
-DibideNumber(number1, number2)
+DivideNumber(number1, number2)
 print("---------------------")
 print("---------------------")
